936f17a9795670f6fd890a65950cc19eaa262aa9
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-18 18:44:47 +0000

    .

--- Carta.js
+++ Carta.js
@@ -1,12 +1,7 @@
 const Boton = document.getElementById("Boton")
-const titulo = document.getElementById("titulo-centrado")
 
 Boton.addEventListener("mousedown", () => {
 Boton.style.backgroundColor="red"
-})
-
-Boton.addEventListener("mousedown", () => {
-titulo.style.color = "red"
 })
 
 
