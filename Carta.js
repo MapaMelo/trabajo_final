bf8c9d6ab2f70544516702efc19549a2e784edc4
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-20 18:24:25 +0000

    ;

--- Carta.js
+++ Carta.js
@@ -4,12 +4,12 @@ const Titulo = document.getElementById("Titulo");
 Boton.addEventListener("mousedown", () => {
     Boton.style.backgroundColor="PeachPuff";
     Titulo.style.color = "PeachPuff";
-});
+})
  
 Boton.addEventListener("mouseup", () => {
     Boton.style.backgroundColor="Black";
     Titulo.style.color = "Black";
-});
+})
 
 const elementos = [
     "Medalla/Prueba_1.jpeg",
