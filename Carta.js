f294dedc2017b16e1f2aa32c797c83fbe0028ee3
MapaMelo <148729836+MapaMelo@users.noreply.github.com>
2025-11-27 01:24:27 +0000

    audio

--- Carta.js
+++ Carta.js
@@ -29,6 +29,7 @@ function Interaccion(elem) {
     }
 
     if (src.endsWith(".mp3") || src.endsWith(".wav")) {
+        elem.style.width = "130px";
         elem.addEventListener("mouseenter", () => elem.play());
         elem.addEventListener("mouseleave", () => {
             elem.pause();
